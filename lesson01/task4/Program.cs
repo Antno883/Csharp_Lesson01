72907b1494845fa1711735f806825be482a604f0
Vankoover <antno883@gmail.com>
2024-06-19 19:50:17 +0300

    add if lesson01:task4

---
+++ lesson01/task4/Program.cs
@@ -0,0 +1,32 @@
+﻿int a = 5;
+int b = 8;
+int c = 6;
+int d = 2;
+int e = 11;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+
+Console.WriteLine(max);
+
+
+
+
+
+
