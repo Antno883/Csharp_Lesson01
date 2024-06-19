6c3ee7f2fe8e3864f8cd8288b642025d45d21188
Vankoover <antno883@gmail.com>
2024-06-19 20:31:35 +0300

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+Console.Clear();
+Console.Write("Input value 1 = ");
+int number=int.Parse(Console.ReadLine());
+Console.WriteLine();
+Console.Write("Input value 2 = ");
+int number2=int.Parse(Console.ReadLine());
+int sqr=number2*number2;
+
+// int sqr1=Convert.ToInt32(Math.Pow(number,2));
+
+if (sqr == number)
+{
+    Console.WriteLine("yes");
+    
+}
+else
+{
+    Console.WriteLine("no");
+    
+}
