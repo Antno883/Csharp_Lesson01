26bcad73d4bfc0886811e02b77f504d8917411e2
Vankoover <antno883@gmail.com>
2024-06-19 20:47:44 +0300

    task3

---
+++ task3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+Console.Clear();
+Console.Write("Input value 1 = ");
+int a=int.Parse(Console.ReadLine());
+int b = -a;
+
+while (b<=a)
+{
+    Console.WriteLine($"{b}");
+    b++;     
+}
+
+
+//for (int i = 1; i < n; i++)
+//{
+//    Console.WriteLine(i);
+//
+//}
