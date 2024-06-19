7eaffac44e7758623bf153edb5d8941b5047218f
Vankoover <antno883@gmail.com>
2024-06-19 20:40:45 +0300

    task2

---
+++ task2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+Console.Clear();
+Console.Write("Input value 1 = ");
+int day=int.Parse(Console.ReadLine());
+
+if (day == 1)
+{
+    Console.WriteLine($"today is monday");
+      
+}
+if (day == 2)
+{
+    Console.WriteLine($"today is teusday");
+      
+}
+if (day == 3)
+{
+    Console.WriteLine($"today is Wednesday");
+      
+}
+if (day == 4)
+{
+    Console.WriteLine($"today is Thursday");
+      
+}
+if (day == 5)
+{
+    Console.WriteLine($"today is Friday");
+      
+}
+if (day == 6)
+{
+    Console.WriteLine($"today is Saturday");
+      
+}
+if (day == 7)
+{
+    Console.WriteLine($"today is Sunday");
+      
+}
