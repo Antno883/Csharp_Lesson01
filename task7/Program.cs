f09280460ee742e0d0f4c4cf302185d24c2c50ee
Vankoover <antno883@gmail.com>
2024-06-20 02:04:14 +0300

    seminar:task7

---
+++ task7/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+Write("Enter first number: ");
+int a=int.Parse(Console.ReadLine());
+Write("Enter second number: ");
+int b=int.Parse(Console.ReadLine()) ;
+
+
+if (a%b==0)
+{
+    WriteLine("First number is divisible by second number");
+
+}
+else
+{
+    WriteLine("second number is not divisible by first number");
+    }
