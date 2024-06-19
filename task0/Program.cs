52b2bf90562a3c651a09f0895c09066027048b78
Vankoover <antno883@gmail.com>
2024-06-19 20:17:24 +0300

    task0

---
+++ task0/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.Write("Input value = ");
+int number=int.Parse(Console.ReadLine());
+int sqr=number*number;
+
+Console.WriteLine($"square number = {sqr}");
+
+int sqr1=Convert.ToInt32(Math.Pow(number,2));
+Console.WriteLine($"square number = {sqr1}");
+
+
