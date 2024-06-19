5168fa2810f4697f56d91ed8d49582605805a910
Vankoover <antno883@gmail.com>
2024-06-20 00:17:45 +0300

    homework:task1

---
+++ homework/task1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine('');
+
+Console.Write();
+int number=int.Parse(Console.ReadLine());
+
+
+if (firstNumber > secondNumber)
+{
+    Console.WriteLine("первое число больше");
+}
+else if (firstNumber < secondNumber)
+{
+    Console.WriteLine("второе число больше");
+}
+else if (firstNumber == secondNumber)
+{
+    Console.WriteLine("числа равны");
+}
