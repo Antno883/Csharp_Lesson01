35e2bbdfadb39cfb0f51bf6c35490ed7e6278687
Vankoover <antno883@gmail.com>
2024-06-20 04:34:19 +0300

    HomeWork:task15

---
+++ lesson04/task15/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Enter number 1-7: ");
+int number = int.Parse(Console.ReadLine());
+
+
+if (number > 7) {
+    WriteLine("Entered number is not 1-7.");
+    return;
+}
+else
+{
+    if (number == 7 || number == 6)
+    {
+        WriteLine("Yes today is Sunday and rest day.");
+    }
+    else
+    {
+        WriteLine("Yes today is a working day.");
+    }
+}
