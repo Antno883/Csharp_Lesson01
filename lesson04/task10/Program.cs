458d6cbee20692086a6fee9dec238d6bf8834035
Vankoover <antno883@gmail.com>
2024-06-20 04:00:43 +0300

    HomeWork:task10

---
+++ lesson04/task10/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using static System.Console;
+
+Clear();
+
+Write("Enter a 3-digital number: ");
+int number = int.Parse(Console.ReadLine());
+
+int hundreds = (number-((number / 100)*100))/10;
+Console.WriteLine($"{hundreds}"); // Display the hundreds digit                       
