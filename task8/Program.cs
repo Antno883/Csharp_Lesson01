7cc28885f2bb31c285128b44ab4aca552a978d31
Vankoover <antno883@gmail.com>
2024-06-20 03:49:57 +0300

    seminar:task8

---
+++ task8/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+
+Write("Enter a number:  ");
+int number=int.Parse(Console.ReadLine());
+
+if (number%7)&&(number%23)
+{
+WriteLine("Number is divisible by 7 and 23");
+}
+else
+{
+    WriteLine("Number is not divisible by 7 and 23");
+}
